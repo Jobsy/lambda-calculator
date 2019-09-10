afbd4fb1d44c612ba63113277a9f7b3b2aa9e3be
Oluwajoba <oluwajobabello@gmail.com>
2019-09-10 19:02:36 +0100

    delete initial imports from data.js and move to the right file / Number.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, {useState} from "react";
-// import Data from "./data.js";
-import { numbers } from './data.js';
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
-// console.log(Data.numbers.length)
-// console.log(numbers.length)
-
-
 
 function App() {
 
-  const [numberState, setNumberState] = useState(numbers);
- 
-  // console.log(numberState)
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { numbers } from './data.js';
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -7,8 +7,12 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
+console.log(numbers.length)
+
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
+ 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
